1dc5ca307e01ec965a0d2525fbedd54329888977
Wei-Jun Hung <weijunhung@outlook.com>
2020-10-06 11:57:29 +0800

    Add token control functions

--- Object-Oriented JavaScript Challenge/js/Game.js
+++ Object-Oriented JavaScript Challenge/js/Game.js
@@ -9,10 +9,10 @@ class Game {
      * Returns active player.
      * @return  {Object} player - The active player.
      */
-	get activePlayer() {
+    get activePlayer() {
         return this.players.find(player => player.active);
     }
-    
+
     /** 
      * Creates two player objects
      * @return  {array} An array of two player objects.
@@ -31,5 +31,42 @@ class Game {
         this.board.drawHTMLBoard();
         this.activePlayer.activeToken.drawHTMLToken();
         this.ready = true;
+    }
+
+    /**
+     * Branches code, depending on what key player presses
+     * @param	{Object} e - Keydown event object
+     */
+    handleKeydown(e) {
+        if (this.ready) {
+            if (e.key === "ArrowLeft") {
+                this.activePlayer.activeToken.moveLeft();
+            } else if (e.key === "ArrowRight") {
+                this.activePlayer.activeToken.moveRight(this.board.columns);
+            } else if (e.key === "ArrowDown") {
+                this.playToken();
+            }
+        }
+    }
+
+    /**
+     * Finds Space object to drop Token into, drops Token
+     */
+    playToken(){
+        let spaces = this.board.spaces;
+        let activeToken = this.activePlayer.activeToken;
+        let targetColumn = spaces[activeToken.columnLocation];
+        let targetSpace = null;
+
+		for (let space of targetColumn) {
+			if (space.token === null) {
+				targetSpace = space;
+			}
+        }
+
+        if (targetSpace !== null) {
+            game.ready = false;
+    		activeToken.drop(targetSpace);   
+        }              
     }
 }
